a8e3bd8e815c44fb2540a1d83cbe1bc0748a64b4
Artur <fhnehbcvfubkjd@yandex.ru>
2024-01-25 14:36:07 +0500

    14-2

---
+++ 14-2.py
@@ -0,0 +1,11 @@
+def space_game(text):
+    a = []
+    for i in range(len(text)):
+        a.append(text[i])
+    b = a.count(' ')
+    if b % 2 == 0:
+        print("Вы выиграли")
+    else:
+        print("Вы проиграли")
+
+print(space_game('я иду с мечем судия'))
