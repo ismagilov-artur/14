0da93f8302bc17278c78b3efb9132fb528ed9a82
Artur <fhnehbcvfubkjd@yandex.ru>
2024-01-25 14:39:58 +0500

    14-9

---
+++ 14-9.py
@@ -0,0 +1,11 @@
+def ask_password():
+    for i in range(0, 3):
+        s = input()
+        if s == 'password':
+            print('Пароль принят.')
+            break
+        elif i == 2:
+            print('В доступе отказано')
+    return
+
+print(ask_password())
